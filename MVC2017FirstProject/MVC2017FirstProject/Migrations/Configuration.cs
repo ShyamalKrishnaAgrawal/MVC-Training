ab1bf10d7067c652876e6edb8db7d88977169242
Shyamal K <shyamalka@apra.in>
2023-01-19 17:57:52 +0530

    add 1st mvc file where three tables are there and we can see the details of student table

---
+++ MVC2017FirstProject/MVC2017FirstProject/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace MVC2017FirstProject.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MVC2017FirstProject.DAL.SchoolContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "MVC2017FirstProject.DAL.SchoolContext";
+        }
+
+        protected override void Seed(MVC2017FirstProject.DAL.SchoolContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ MVC2017FirstProject/MVC2017FirstProject/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC2017FirstProject.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        public bool isVacinated { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
